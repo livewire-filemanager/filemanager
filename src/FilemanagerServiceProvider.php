8919d93d78136c2d46b31ef83cde32ace8d385a2
Yves Engetschwiler <info@bee-interactive.ch>
2023-11-21 07:46:44 +0100

    Base Service Provider file

---
+++ src/FilemanagerServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace LivewireFilemanager\Filemanager;
+
+use Illuminate\Support\ServiceProvider;
+
+class LivewireChartsServiceProvider extends ServiceProvider
+{
+    public function boot() {}
+
+    public function register() {}
+}
