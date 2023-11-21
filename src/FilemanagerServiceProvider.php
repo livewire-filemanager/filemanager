c709b73312b9f4805d182497bd970d7348a14f3c
Yves Engetschwiler <info@bee-interactive.ch>
2023-11-21 14:41:48 +0100

    Base configuration for FilemanagerServiceProvider

--- src/FilemanagerServiceProvider.php
+++ src/FilemanagerServiceProvider.php
@@ -2,11 +2,39 @@
 
 namespace LivewireFilemanager\Filemanager;
 
+use Livewire\Livewire;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use LivewireFilemanager\Filemanager\Livewire\LivewireFilemanagerComponent;
+use LivewireFilemanager\Filemanager\Http\Components\BladeFilemanagerComponent;
 
-class LivewireChartsServiceProvider extends ServiceProvider
+class FilemanagerServiceProvider extends ServiceProvider
 {
-    public function boot() {}
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'livewire-filemanager');
 
-    public function register() {}
+        $this
+            ->registerBladeComponents()
+            ->registerLivewireComponents();
+    }
+
+    public function register()
+    {
+        parent::register();
+    }
+
+    public function registerLivewireComponents(): self
+    {
+        Livewire::component('livewire-filemanager', LivewireFilemanagerComponent::class);
+
+        return $this;
+    }
+
+    public function registerBladeComponents(): self
+    {
+        Blade::component('livewire-filemanager', BladeFilemanagerComponent::class);
+
+        return $this;
+    }
 }
