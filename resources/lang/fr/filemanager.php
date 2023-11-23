1524d6841999ece05d18abe5e97d1747a48e01fa
Yves Engetschwiler <info@bee-interactive.ch>
2023-11-23 17:29:29 +0100

    Updated translations

--- resources/lang/fr/filemanager.php
+++ resources/lang/fr/filemanager.php
@@ -2,5 +2,12 @@
 
 return [
     'root_folder_not_configurated' => 'Votre dossier de départ n’est pas configuré.',
+    'root_folder_not_configurated_help' => 'Ce dossier sera votre dossier de départ de votre gestionnaire de fichiers.',
     'add_your_first_folder' => 'Ajoutez votre premier dossier.',
+    'actions' => [
+        'cancel' => 'Annuler',
+        'save' => 'Enregistrer',
+        'delete' => 'Supprimer',
+        'close_modal' => 'Fermer la popup',
+    ],
 ];
