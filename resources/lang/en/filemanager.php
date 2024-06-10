c23100c9309fc27b2902b30896b188fdeb5ff6f2
Yves Engetschwiler <info@bee-interactive.ch>
2024-06-10 15:42:29 +0200

    Updated translations

---
+++ resources/lang/en/filemanager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'root_folder_not_configurated' => 'Your root folder is not configured',
+    'root_folder_not_configurated_help' => 'This folder will be your root folder of your file manager',
+    'add_your_first_folder' => 'Add your first folder',
+    'root_folder_name' => 'Folder name',
+    'elements' => '{0} No elements|[1] 1 element|[2,*] :value elements',
+    'add_a_file' => 'Add a file',
+    'search' => 'Search',
+    'folder_already_exists' => 'A folder with this name already exists in the current directory.',
+    'folder_without_title' => 'untitled folder',
+    'actions' => [
+        'cancel' => 'Cancel',
+        'save' => 'Save',
+        'delete' => 'Delete',
+        'close_modal' => 'Close modal',
+    ],
+    'validation' => [
+        'folder_name_required' => 'The folder name is required',
+    ]
+];
--- resources/lang/fr/filemanager.php
+++ resources/lang/fr/filemanager.php
@@ -1,10 +1,15 @@
 <?php
 
 return [
-    'root_folder_not_configurated' => 'Votre dossier de départ n’est pas configuré.',
-    'root_folder_not_configurated_help' => 'Ce dossier sera votre dossier de départ de votre gestionnaire de fichiers.',
-    'add_your_first_folder' => 'Ajoutez votre premier dossier.',
+    'root_folder_not_configurated' => 'Votre dossier de départ n’est pas configuré',
+    'root_folder_not_configurated_help' => 'Ce dossier sera votre dossier de départ de votre gestionnaire de fichiers',
+    'add_your_first_folder' => 'Ajoutez votre premier dossier',
     'root_folder_name' => 'Intitulé de votre dossier de départ',
+    'elements' => '{0} Aucun élément|[1] 1 élément|[2,*] :value éléments',
+    'add_a_file' => 'Ajouter un fichier',
+    'search' => 'Rechercher',
+    'folder_already_exists' => 'Le dossier existe déjà',
+    'folder_without_title' => 'dossier sans titre',
     'actions' => [
         'cancel' => 'Annuler',
         'save' => 'Enregistrer',
@@ -12,6 +17,6 @@ return [
         'close_modal' => 'Fermer la popup',
     ],
     'validation' => [
-        'folder_name_required' => 'Le nom du dossier est obligatoire.',
+        'folder_name_required' => 'Le nom du dossier est obligatoire',
     ]
 ];
