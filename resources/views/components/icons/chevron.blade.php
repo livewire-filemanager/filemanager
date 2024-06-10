7e8826ca76fbd8bdb2e030988d0e96a2f0fc8458
Yves Engetschwiler <info@bee-interactive.ch>
2024-06-10 17:13:01 +0200

    Added chevron icon

---
+++ resources/views/components/icons/chevron.blade.php
@@ -0,0 +1,7 @@
+<svg class="w-2 h-2" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 48 48" style="enable-background:new 0 0 48 48;" xml:space="preserve">
+    <style type="text/css">
+        .st0{fill:none;}
+    </style>
+    <path d="M15.7,0.8l-5.5,5.5L28,24L10.2,41.8l5.5,5.5L39,24L15.7,0.8z"/>
+    <path class="st0" d="M0,0h48v48H0V0z"/>
+</svg>
