b4a3a18b3a1679e15c6f59c90f4042690307b4a3
Yves Engetschwiler <info@bee-interactive.ch>
2024-06-11 16:05:20 +0200

    Added delete items behavior

--- src/Livewire/DeleteItemsComponent.php
+++ src/Livewire/DeleteItemsComponent.php
@@ -3,11 +3,39 @@
 namespace LivewireFilemanager\Filemanager\Livewire;
 
 use Livewire\Component;
+use Livewire\Attributes\On;
+use LivewireFilemanager\Filemanager\Models\Folder;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class DeleteItemsComponent extends Component
 {
-    public function mount(array $items)
+    public $files;
+
+    public $folders;
+
+    #[On('delete-items')]
+    public function deleteItems(array $folders, array $files)
     {
-        dd($items);
+        $this->folders = $folders;
+
+        $this->files = $files;
+    }
+
+    public function delete()
+    {
+        foreach ($this->files as $file) {
+            Media::find($file)->delete();
+        }
+
+        foreach ($this->folders as $folder) {
+            Folder::find($folder)->delete();
+        }
+
+        $this->dispatch('reset-media');
+    }
+
+    public function render()
+    {
+        return view('livewire-filemanager::livewire.delete-items');
     }
 }
