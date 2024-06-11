0700805506b4955ee6c850fb80581c0bb1530bb8
Yves Engetschwiler <info@bee-interactive.ch>
2024-06-11 14:07:55 +0200

    Added FileController for public routes access

---
+++ src/Http/Controllers/Files/FileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LivewireFilemanager\Filemanager\Http\Controllers\Files;
+
+use LivewireFilemanager\Filemanager\Models\Folder;
+
+class FileController
+{
+    public function show($path)
+    {
+        $segments = explode('/', $path);
+        $fileName = array_pop($segments);
+
+        $folderPath = implode('/', $segments);
+
+        $folder = Folder::where('slug', '=', end($segments))->firstOrFail();
+
+        $fullFolderPath = buildFolderPath($folder->id);
+
+        if ($folderPath !== $fullFolderPath) {
+            abort(404);
+        }
+
+        $file = $folder->media()->where('file_name', $fileName)->orWhere('name', $fileName)->firstOrFail();
+
+        $filePath = $file->getPath();
+
+        if (!file_exists($filePath)) {
+            abort(404, 'File not found on server');
+        }
+
+        $fileMimeType = mime_content_type($filePath);
+
+        if (in_array($fileMimeType, ['image/jpeg', 'image/png', 'image/gif', 'application/pdf'])) {
+            return response()->file($filePath);
+        }
+
+        return response()->download($filePath, $fileName);
+    }
+}
