eac90925181ea8ad9503b75166a19f209ab9a481
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-12-10 07:35:40 +0330

    Add Persian language file for file manager translations
    This commit introduces a new language file for Persian translations of the file manager interface. It includes key translations related to folder management, file actions, and validation messages to support Persian-speaking users.

---
+++ resources/lang/fa/filemanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'root_folder_not_configurated' => 'پوشه اصلی تنظیم نشده است.',
+    'root_folder_not_configurated_help' => 'این پوشه در واقع حاوی فایل های است که توسط مدیریت فایل نمایش داده می شود.',
+    'add_your_first_folder' => 'افزودن اولین پوشه',
+    'root_folder_name' => 'نام پوشه',
+    'elements' => '{0} بدون المان|[1] 1 المان|[2,*] :value المان',
+    'add_a_file' => 'افزودن فایل',
+    'search' => 'جستجو',
+    'folder_already_exists' => 'پوشه با این نام موجود است.',
+    'folder_without_title' => 'پوشه بدون عنوان',
+    'selected' => '[1] گزینه|[2,*] گزینه',
+    'search_results' => 'نتیجه جستجو',
+    'created' => 'ایجاد شد',
+    'modified' => 'ویرایش شد',
+    'delete_items' => 'حذف آیتم ها',
+    'delete_items_warning' => 'آیا از حذف آیتم ها اطمینان دارید؟',
+    'actions' => [
+        'cancel' => 'لغو',
+        'save' => 'ذخیره',
+        'delete' => 'حذف',
+        'close_modal' => 'بستن پنجره',
+        'copy_url' => 'کپی آدرس',
+        'url_copy_pasted' => 'آدرس کپی شد.',
+    ],
+    'validation' => [
+        'folder_name_required' => 'نام پوشه اجباری است.',
+    ],
+];
