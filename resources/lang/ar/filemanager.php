bd4810773c77af7b73fec98a4bf8e093ff4e2153
hdaklue <hassan@daklue.com>
2024-12-22 03:59:17 +0200

    * Adding Arabic Language Support

---
+++ resources/lang/ar/filemanager.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'root_folder_not_configurated' => 'المجلد الرئيسي غير معرف.',
+    'root_folder_not_configurated_help' => 'هذا المجلد سيكون المجلد الرئيسي لمدير ملفاتك',
+    'add_your_first_folder' => 'اضف اول ملف لمجلدك',
+    'root_folder_name' => 'اسم المجلد',
+    'elements' => '{0} لا توجد عناصر|[1] عنصر واحد|[2,*] :value عناصر',
+    'add_a_file' => 'اضف ملف',
+    'search' => 'البحث',
+    'folder_already_exists' => 'يوجد مجلد بهذا الاسم موجود بالفعل',
+    'folder_without_title' => 'مجلد بدون اسم',
+    'selected' => '[1] تم تحديده|[2,*] تم تحديد',
+    'search_results' => 'نتائج البحث',
+    'created' => 'تم الانشاء',
+    'modified' => 'تم التعديل',
+    'delete_items' => 'حذف العناصر',
+    'delete_items_warning' => 'هل انت متأكد من حذف هذه العناصر؟',
+    'actions' => [
+        'cancel' => 'الغاء',
+        'save' => 'حفظ',
+        'delete' => 'حذف',
+        'close_modal' => 'اغلاق',
+        'copy_url' => 'انسخ رابط الملف',
+        'url_copy_pasted' => 'تم نسخ الرابط',
+    ],
+    'validation' => [
+        'folder_name_required' => 'اسم المجلد مطلوب',
+    ],
+];
