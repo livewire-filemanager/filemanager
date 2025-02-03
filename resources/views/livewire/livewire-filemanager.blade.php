fa432a72da406b4f81c9a1fe49155da9cb0840cb
zvizvi <5633910@gmail.com>
2025-02-03 04:01:19 +0200

    Back button RTL

--- resources/views/livewire/livewire-filemanager.blade.php
+++ resources/views/livewire/livewire-filemanager.blade.php
@@ -45,7 +45,7 @@
                             @if ($this->currentFolder->id !== 1)
                                 <div>
                                     <button class="border rounded p-1.5 border-zinc-300 dark:border-zinc-600 dark:text-zinc-500" @click="Livewire.dispatch('reset-media', { media_id: null })" wire:click="navigateToParent">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 rtl:rotate-y-180">
                                             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                                         </svg>
                                     </button>
